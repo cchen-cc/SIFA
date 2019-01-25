7d069ddc6b43652fd39c19f410ec83781ab6ada7
cchen-cc <44825962+cchen-cc@users.noreply.github.com>
2019-01-25 08:22:37 +0800

    Add files via upload

---
+++ create_datalist.py
@@ -0,0 +1,19 @@
+import os
+import numpy as np
+
+
+def make_datalist(data_fd, data_list):
+    filename_all = os.listdir(data_fd)
+    filename_all = [data_fd+'/'+filename+'\n' for filename in filename_all if filename.endswith('.tfrecords')]
+
+    np.random.shuffle(filename_all)
+    np.random.shuffle(filename_all)
+    with open(data_list, 'w') as fp:
+        fp.writelines(filename_all)
+
+
+if __name__ == '__main__':
+    data_fd = './data/training_A'
+    data_list = './data/datalist/training_A.txt'
+
+    make_datalist(data_fd, data_list)
